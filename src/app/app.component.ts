9261c87f5995c0e6eac3f93b29ffc9338503fe50
jcamacaro <davidjcamacaro@gmail.com>
2024-04-23 02:16:21 -0400

    Aprendisaje del ngFor-ngIf   boton ocultar y mostrar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  selector: 'app-root',
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'introduccion';
+  title = 'Hola mundo de Angular desde Componente!';
+
+  user: string[] = ['jesus','david','camacaro','ruiz'];
+
+
+visible: boolean = false;
+
+setvisible() : void {
+  this.visible = this.visible? false: true;
+}
+
 }
