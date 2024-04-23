e64aa1c900e8e5d3ce5f391a098c8b28381f90e5
jcamacaro <davidjcamacaro@gmail.com>
2024-04-23 11:10:39 -0400

    add componet Contado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CounterComponent } from './counter/counter.component';
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  imports: [RouterOutlet,CommonModule],
+  imports: [RouterOutlet,CounterComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
   title = 'Hola mundo de Angular desde Componente!';
 
-  user: string[] = ['jesus','david','camacaro','ruiz'];
+  user: string[] = ['jesus', 'david', 'camacaro', 'ruiz'];
 
+  visible: boolean = true;
 
-visible: boolean = false;
-
-setvisible() : void {
-  this.visible = this.visible? false: true;
-}
-
+  setvisible(): void {
+    this.visible = this.visible ? false : true;
+  }
 }
